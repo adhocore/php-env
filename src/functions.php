498049755ec7a3237b8c758a5004507b39c69201
Jitendra Adhikari <jiten.adhikary@gmail.com>
2017-10-22 13:58:23 +0700

    feat: function alias for Retriever::getEnv()

---
+++ src/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+// @codeCoverageIgnoreStart
+if (!function_exists('env')) {
+    /**
+     * Get the env variable by its key/name.
+     *
+     * @param  string    $key
+     * @param  mixed     $default
+     * @param  int|null  $filter   PHP's filter constants. See http://php.net/filter_var
+     * @param  int|array $options  Additional options to filter.
+     *
+     * @return mixed
+     */
+    function env($key, $default = null, $filter = null, $options = null)
+    {
+        return \Ahc\Env\Retriever::getEnv($key, $default, $filter, $options);
+    }
+}
+// @codeCoverageIgnoreEnd
