2b0fc594c8987cfe04f99b1cce565a9c2e6298ad
Jitendra Adhikari <jiten.adhikary@gmail.com>
2017-10-22 13:59:47 +0700

    test: Retriever

---
+++ tests/RetrieverTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Ahc\Env\Test;
+
+use Ahc\Env\Retriever;
+
+class RetrieverTest extends \PHPUnit_Framework_TestCase
+{
+    public function testLoad()
+    {
+        putenv('TEST=true');
+        putenv('REST=false');
+        putenv('NULL=null');
+        putenv('ADMIN_EMAIL=admin@example.com');
+        putenv('MAX_LIMIT=1000');
+
+        $this->assertTrue(Retriever::getEnv('TEST'));
+        $this->assertFalse(Retriever::getEnv('REST'));
+        $this->assertNull(Retriever::getEnv('NULL'));
+        $this->assertEquals(
+            'admin@example.com',
+            Retriever::getEnv('ADMIN_EMAIL', null, FILTER_VALIDATE_EMAIL)
+        );
+        $this->assertSame(1000, Retriever::getEnv('MAX_LIMIT', null, FILTER_VALIDATE_INT));
+
+        $_SERVER += [
+            'USE_CYPHER' => 'true',
+        ];
+
+        $this->assertTrue(Retriever::getEnv('USE_CYPHER'));
+
+        $_ENV += [
+            'DEFAULT_PAYMENT' => 'paypal',
+        ];
+
+        $this->assertSame('paypal', Retriever::getEnv('DEFAULT_PAYMENT'));
+    }
+}
